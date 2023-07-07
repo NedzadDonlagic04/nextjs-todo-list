5c6fdcb3d91478fdb4da82f73a25266853bde83e
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-07 23:56:33 +0200

    Added link to lead to new path and gave it basic style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
         <>
             <header>
                 <h1 className="text-4xl font-bold">Todo List</h1>
             </header>
+
+            <Link href='/new' className="mt-5 border-2 border-black rounded-lg px-4 py-2 float-right transition-all duration-300 ease-in-out hover:bg-sky-200 hover:translate-y-[-8px]">New</Link>
         </>
     );
 }
