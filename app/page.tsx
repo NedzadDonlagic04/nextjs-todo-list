9f240a5dd7f0f6867fde01ef93ddb3ea7311cf6e
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-08 08:51:50 +0200

    Added function to get todoItems and display them

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
+import TodoItem from "@components/TodoItem";
+import { prisma } from "@db/db";
 import Link from "next/link";
 
-export default function Home() {
+function getTodoItems() {
+    return prisma.todo.findMany();
+}
+
+export default async function Home() {
+    const todos = await getTodoItems();
+
     return (
         <>
             <header>
                 <h1 className="text-4xl font-bold">Todo List</h1>
             </header>
 
-            <Link href='/new' className="mt-5 border-2 border-black rounded-lg px-4 py-2 float-right transition-all duration-300 ease-in-out hover:bg-sky-200 hover:translate-y-[-8px]">New</Link>
+            <ul className="mt-5">
+                {todos.map(todo => (
+                    <TodoItem key={todo.id} {...todo} />
+                ))}
+            </ul>
+            
+            <Link href='/new' className="mt-5 border-2 border-black rounded-lg px-4 py-2 float-right transition-all duration-300 ease-in-out hover:bg-sky-200 hover:translate-y-[-8px] focus:bg-sky-200 focus:translate-y-[-8px]">New</Link>
         </>
     );
 }
