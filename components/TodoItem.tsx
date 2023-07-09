e24cb48e0655b35b9a8a49aea303df4c014b6236
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-09 16:54:29 +0200

    Added TodoItem component

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,13 +1,22 @@
-type TodoItemProps = {
-    id: string
-    title: string
-    complete: Boolean
-}
+"use client"
 
-export default function TodoItem({ id, title, complete } : TodoItemProps) {
+type TodoItemProps = {
+  id: string
+  title: string
+  complete: boolean
+  toggleTodo: (id: string, complete: boolean) => void
+};
+
+export default function TodoItem({ id, title, complete, toggleTodo } : TodoItemProps) {
   return (
     <li className="flex gap-2 items-center">
-        <input type="checkbox" id={id} className="cursor-pointer peer" />
+        <input 
+          type="checkbox" 
+          id={id} 
+          className="cursor-pointer peer" 
+          defaultChecked={complete}  
+          onChange={e => toggleTodo(id, e.target.checked)}
+        />
         <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">
             {title}
         </label>
