97b25bd8b7c90afcec87a9e717941ad7ed8519dd
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-07-09 16:58:42 +0200

    Added function that'll be used by a TodoItem

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,19 @@ function getTodoItems() {
     return prisma.todo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+    "use server"
+
+    await prisma.todo.update({
+        where: {
+            id
+        }, 
+        data: {
+            complete
+        }
+    });
+}
+
 export default async function Home() {
     const todos = await getTodoItems();
 
@@ -17,11 +30,11 @@ export default async function Home() {
 
             <ul className="mt-5">
                 {todos.map(todo => (
-                    <TodoItem key={todo.id} {...todo} />
+                    <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
                 ))}
             </ul>
             
-            <Link href='/new' className="mt-5 border-2 border-black rounded-lg px-4 py-2 float-right transition-all duration-300 ease-in-out hover:bg-sky-200 hover:translate-y-[-8px] focus:bg-sky-200 focus:translate-y-[-8px]">New</Link>
+            <Link href='/new' className="btn">New</Link>
         </>
     );
 }
